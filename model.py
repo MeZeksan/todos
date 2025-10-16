8fddd6031964c4e7b59dbddefd571168c34922dd
булат закиров <bulzak74848@gmail.com>
2025-10-16 15:51:31 +0300

    Практика 5

--- model.py
+++ model.py
@@ -10,3 +10,29 @@ class Todo(BaseModel):
                 "item": "Example schema!"
             }
         }
+
+class TodoItem(BaseModel):
+    item: str
+    
+    class Config:
+        schema_extra = {
+            "example": {
+                "item": "Read the next chapter of the book"
+            }
+        }
+class TodoItems(BaseModel):
+    todos: list[TodoItem]
+    
+    class Config:
+        schema_extra = {
+            "example": {
+                "todos": [
+                    {
+                        "item": "Example schema 1!"
+                    },
+                    {
+                        "item": "Example schema 2!"
+                    }
+                ]
+            }
+        }
