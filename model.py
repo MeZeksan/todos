4a3f636327b94e297a6e1f47d659fbc566639682
булат закиров <bulzak74848@gmail.com>
2025-10-16 15:16:21 +0300

    Практика 4

---
+++ model.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel
+class Todo(BaseModel):
+    id: int
+    item: str
+
+    class Config:
+        Schema_extra = {
+            "example": {
+                "id": 1,
+                "item": "Example schema!"
+            }
+        }
