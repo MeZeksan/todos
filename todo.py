78d147359c2f0c0de527a818fdddf609f1d1bc93
MeZeksan <bulzak74848@gmail.com>
2025-10-23 10:16:43 +0500

    Исправил ошибки

--- todo.py
+++ todo.py
@@ -12,14 +12,14 @@ async def add_todo(request: Request, todo: Todo =
     Depends(Todo.as_form)):
         todo.id = len(todo_list) + 1
         todo_list.append(todo)
-            return templates.TemplateResponse("todo.html",
-                {
-                    "request": request,
-                    "todos": todo_list
-                })
+        return templates.TemplateResponse("todo.html",
+            {
+                "request": request,
+                "todos": todo_list
+            })
 
 @todo_router.get("/todo", response_model=TodoItems)
-async def retrieve_todo() -> dict:
+async def retrieve_todo(request: Request) -> dict:
     return templates.TemplateResponse("todo.html", {
         "request": request,
         "todos": todo_list
@@ -27,7 +27,7 @@ async def retrieve_todo() -> dict:
 
 
 @todo_router.get("/todo/{todo_id}")
-async def get_single_todo(todo_id: int = Path(..., title="The ID of the todo to retrieve.")) -> dict:
+async def get_single_todo(request: Request, todo_id: int = Path(..., title="The ID of the todo to retrieve.")) -> dict:
     for todo in todo_list:
         if todo.id == todo_id:
             return templates.TemplateResponse(
